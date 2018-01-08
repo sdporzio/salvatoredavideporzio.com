a7a7a76b2a9156fc329158a0eecf9f003646d4c9
Davide Porzio <salvatore.porzio@postgrad.manchester.ac.uk>
2018-01-08 15:41:40 +0000

    automatic update ${DATETIME}

---
+++ HouseEnvironment/makePlots.py
@@ -0,0 +1,26 @@
+import os, sys, json, pickle
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from collections import OrderedDict as OD
+from NotebookUtils.ProgressBar import LogProgress as LP
+from skhep.simulation import pdgid
+plt.rcParams['font.family']='serif'
+plt.rcParams['font.weight']='light'
+plt.rcParams['font.size']=14
+figsize = (12,8)
+
+import datetime;
+now = datetime.datetime.now()
+
+time = []
+temp = []
+last = now
+for i in range(50):
+    last = last + datetime.timedelta(seconds=60)
+    time.append(last)
+    temp.append(np.random.rand()*2. + 20)
+
+fig = plt.figure(figsize=figsize)
+plt.plot(time,temp)
+plt.savefig('HouseEnvironments/Plots/temperature.jpg')
---
+++ update.sh
@@ -0,0 +1,6 @@
+DATETIME="$(date +%d%m%y%H%M%S)"
+
+python HouseEnvironment/makePlots.py
+git add .
+git commit -m 'automatic update ${DATETIME}'
+git push origin master
